f0bf5d1df4cada4db6206d926ae2511bb5a9be4d
liskator <206425368+liskator@users.noreply.github.com>
2025-05-20 20:03:02 +0300

    Update history.js

--- history.js
+++ history.js
@@ -17,7 +17,7 @@
                 onMain: (data) => {
                     return {
                     title:['История'],
-                    results:JSON.parse(localStorage.getItem('favorite')).card.reverse(),
+                    results:JSON.parse(localStorage.getItem('favorite')).card.slice(0,19).reverse(),
                     }
                 }
             };
