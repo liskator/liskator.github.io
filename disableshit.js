9178b6e2da72903380bc18faed4bba4a7b106f44
liskator <206425368+liskator@users.noreply.github.com>
2025-10-26 15:35:29 +0200

    Update disableshit.js

--- disableshit.js
+++ disableshit.js
@@ -8,7 +8,7 @@
 
     function httpGet(theUrl) {
         var xmlHttp = new XMLHttpRequest();
-        xmlHttp.open("GET", theUrl); // false for synchronous request
+        xmlHttp.open("GET", theUrl,true); // false for synchronous request
         xmlHttp.send(null);
         return xmlHttp.responseText;
     }
